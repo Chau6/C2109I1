d3bfaa830e27631dcb28621803c4e02341119de8
Chau6 <tanchau0602@gmail.com>
2022-12-16 13:23:16 +0700

    try catch

---
+++ C2109I1/RegexTryParse/Program.cs
@@ -0,0 +1,53 @@
+﻿//bắt lỗi
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+string? str = null;
+//Console.WriteLine("Vui lòng nhập số: ");
+//str = Console.ReadLine();
+
+var formula = new Regex("^[0-9]+$");
+
+//làm web
+if (formula.IsMatch(str))
+{
+    //int result = int.Parse(str);
+    int result = Convert.ToInt32(str);
+    Console.WriteLine($"Result = {result}");
+}
+
+//làm console
+if (int.TryParse(str, out int i))
+{
+    Console.WriteLine($"{nameof(i)} = {i}");
+}
+
+Console.WriteLine("Vui lòng nhập số lượng: ");
+string? amount = Console.ReadLine(); ;
+//dùng try catch
+//bảo vệ mệnh đề "guard clause"
+if (string.IsNullOrEmpty(amount))
+{
+    return; //ngắt luôn nếu sai
+}
+
+//do something
+
+if(amount is not null)
+{
+    //do something
+}
+
+try
+{
+    int total = int.Parse(amount);
+    if (total <= 0) throw new Exception("Phải lớn hơn 0");
+    Console.WriteLine($"{nameof(total)} = {total}");
+}
+catch(Exception e)
+{
+    Console.WriteLine($"{e.GetType()}, {e.Message}");
+}
